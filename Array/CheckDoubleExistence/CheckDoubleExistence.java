e37b292d63629df0ea06fced8ce6419e9e3a55f7
Alex Tao <t.ruizhe@gmail.com>
2021-03-14 20:30:00 +0100

    Believed to be a very bad solution

--- Array/CheckDoubleExistence/CheckDoubleExistence.java
+++ Array/CheckDoubleExistence/CheckDoubleExistence.java
@@ -1,5 +1,21 @@
 public class CheckDoubleExistence {
 
 	public boolean checkIfExist(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+			    if (arr[i] == 0) {
+			    	if (arr[j] == 0 && i != j) {
+			    		return true;
+					}
+				}
+				if (arr[j] == 0) {
+					continue;
+				} else if (arr[j] == 2 * arr[i]) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
