24c7d2db5242f3d7fdc441b885fbe8f0d45763bc
Alex Tao <t.ruizhe@gmail.com>
2021-03-16 22:26:13 +0100

    Better Solution with One Pass

--- Linked List/RemoveNthNodeFromEndofList/src/Solution.java
+++ Linked List/RemoveNthNodeFromEndofList/src/Solution.java
@@ -1,28 +1,23 @@
 public class Solution {
 
-    public int getLength(ListNode head) {
-        int count = 0;
-
-        while (head != null) {
-            count++;
-            head = head.next;
-        }
-
-        return count;
-    }
-
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode prev = dummy;
-        int length = getLength(head);
-        int prevIndex = length - n;
+        int size = 1;
+        ListNode cur = head;
+        ListNode pos = cur;
 
-        while (prevIndex > 0) {
-            prevIndex--;
-            prev = prev.next;
+        while (cur.next != null) {
+            size++;
+            cur = cur.next;
+
+            if (size > n + 1) {
+                pos = pos.next;
+            }
         }
 
-        prev.next = prev.next.next;
-        return dummy.next;
+        if (size == n) {
+            return head.next;
+        }
+        pos.next = pos.next.next;
+        return head;
     }
 }
